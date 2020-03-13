aa1307865cefa0d8292cecacbad8f0ad0ca52ec4
Emiliano Gnocchi <emilianog@oktana.io>
2020-03-12 21:29:41 -0300

    Programa java!!

---
+++ ProyectosJava/src/Ejercicios/prueba.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Estamos en clase");
+	}
+
+}
