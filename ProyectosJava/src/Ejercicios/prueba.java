306b3339bdfba4252432333980fa2c1235c51be3
Emiliano Gnocchi <emilianog@oktana.io>
2020-03-12 21:39:09 -0300

    Borrando la linea de comentarios

--- ProyectosJava/src/Ejercicios/prueba.java
+++ ProyectosJava/src/Ejercicios/prueba.java
@@ -3,7 +3,6 @@ package Ejercicios;
 public class prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Seguimos en clase!!!");
 		int x = 4;
